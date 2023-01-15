c8a6290ec9d00c8a3fe554f80e0d9b83064d884c
gafayo <gafayoabel20@gmail.com>
2023-01-15 19:50:00 +0300

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+const bar = document.querySelector('.menu-bar');
+const links = document.querySelector('.list-of-links');
+console.log(bar)
+bar.addEventListener('click', function(){
+    console.log('hos');
+})
+
+// bar.addEventListener('click', ()=>{
+//     console.log('clicked')
+// })
